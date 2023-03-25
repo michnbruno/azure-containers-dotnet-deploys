25e4fad8577b973ea897b861138b3c33a683cb5c
Michael Bruno <michnbruno@gmail.com>
2023-03-25 14:06:54 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# azure-containers-dotnet-deploys3
+# azure-containers-dotnet-deploys
+
+Deploy of .NET Apps to Azure Container Apps.
+
+Demonstrates:
+1. Docker image builds
+2. Provisioning of Azure PaaS Resources - Environment, Container Apps, Analytics
+3. Github Workflows/Actions to Provision and subsequent CI/CD
+
+Many thanks to the ASP.NET Core Front-end + 2 Back-end APIs on Azure Container Apps contributors for this fine walkthrough.
+(original README.md included as well) 
---
+++ deploy.ps1
@@ -0,0 +1,19 @@
+# *nix only
+
+$SUB_ID=""
+
+$RESOURCE_GROUP_NAME= "storemnb6"
+
+# specify your preferred region
+$REGION= "eastus"
+
+
+# Follow Azure CLI prompts to authenticate to your subscription of choice
+az login
+az account set --subscription $SUB_ID
+
+# Create resource group
+az group create -n $RESOURCE_GROUP_NAME -l $REGION
+
+# Deploy all infrastructure and reddog apps
+ az deployment group create --resource-group $RESOURCE_GROUP_NAME --template-file './Azure/storemnb/main.bicep' --debug
